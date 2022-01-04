e022a61b7e068f30bca72a7ac0583dad1aaf596f
Nicoorfi <nico@sigmie.com>
2022-01-04 13:31:05 +0100

    UPD: fallback to syntax higthlighter

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import ReactMarkdown from "react-markdown";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
-// import torchlight from 'remark-torchlight' 
+import style from "react-syntax-highlighter/dist/cjs/styles/prism/dracula";
 
 import { Layout, Image, SEO, Bio } from "@components/common";
 import { getPostBySlug, getPostsSlugs } from "@utils/posts";
@@ -27,11 +27,24 @@ export default function Post({ post, frontmatter, nextPost, previousPost }) {
         <ReactMarkdown
           className="mb-4 prose lg:prose-lg font-"
           skipHtml={false}
-    //       remarkPlugins={[torchlight,{
-    // token: 'torch_ImyRgG5kK5xOpXpgqbMtq8ZrYDQLG18iVFlL4O85',
-    //             theme: 'material-theme-palenight'
-
-    //       }]}
+    components={{
+      code({node, inline, className, children, ...props}) {
+        const match = /language-(\w+)/.exec(className || '')
+        return !inline && match ? (
+          <SyntaxHighlighter
+            children={String(children).replace(/\n$/, '')}
+            style={style}
+            language={match[1]}
+            PreTag="div"
+            {...props}
+          />
+        ) : (
+          <code className={className} {...props}>
+            {children}
+          </code>
+        )
+      }
+    }}
           children={post.content}
         />
         <hr className="mt-4" />
@@ -85,19 +98,3 @@ export async function getStaticProps({ params: { slug } }) {
 
   return { props: postData };
 }
-
-const CodeBlock = ({ language, value }) => {
-  return (
-    <SyntaxHighlighter language={language}>
-      {value}
-    </SyntaxHighlighter>
-  );
-};
-
-const MarkdownImage = ({ alt, src }) => (
-  <img
-    alt={alt}
-    src={`../../content/assets/${src}`}
-    className="w-full"
-  />
-);
